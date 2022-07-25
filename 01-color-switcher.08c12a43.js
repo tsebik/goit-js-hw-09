24eee40347fab1332356156980d1402ce70898c3
tsebik <95105459+tsebik@users.noreply.github.com>
2022-07-25 14:58:20 +0000

    Deploying to gh-pages from @ tsebik/goit-js-hw-09@e42eee580556964ef70a44ffcf078ec22e7a084b 🚀

--- 01-color-switcher.08c12a43.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");console.log(o),t.addEventListener("click",(function(){console.log(n=setInterval(l,1e3)),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1})),o.addEventListener("click",l);let n=null;function l(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}
-//# sourceMappingURL=01-color-switcher.08c12a43.js.map
